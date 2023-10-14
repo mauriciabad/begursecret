cde826512631c6f1a2b5a75242ba1e8497c4d6d4
mauriciabad <hello@mauriciabad.com>
2023-10-14 02:38:58 +0200

    rand fix

--- src/app/[locale]/_components/bottom-navbar.tsx
+++ src/app/[locale]/_components/bottom-navbar.tsx
@@ -1,31 +1,32 @@
-import Link from "next-intl/link"
-import { IconCompass, IconAward, IconGift, IconUser } from "@tabler/icons-react"
+import Link from 'next-intl/link'
+import { IconCompass, IconAward, IconGift, IconUser } from '@tabler/icons-react'
+import { FC, PropsWithChildren } from 'react'
 
-export const BottomNavbar =()=> {
+export const BottomNavbar = () => {
   return (
-  <nav className="fixed bottom-0 inset-x-0 h-12 bg-white shadow-xl">
-    <ul className="grid grid-cols-4">
-      <Item url="/explore" >
-        <IconCompass/>
-      </Item>
-      <Item url="/missions" >
-        <IconAward/>
-      </Item>
-      <Item url="/hub" >
-        <IconGift/>
-      </Item>
-      <Item url="/profile" >
-        <IconUser/>
-      </Item>
-    </ul>
-  </nav>
+    <nav className="fixed inset-x-0 bottom-0 h-12 bg-white shadow-xl">
+      <ul className="grid grid-cols-4">
+        <Item url="/explore">
+          <IconCompass />
+        </Item>
+        <Item url="/missions">
+          <IconAward />
+        </Item>
+        <Item url="/hub">
+          <IconGift />
+        </Item>
+        <Item url="/profile">
+          <IconUser />
+        </Item>
+      </ul>
+    </nav>
   )
 }
 
-const Item = ({ children, url }) => {
+const Item: FC<PropsWithChildren<{ url: string }>> = ({ children, url }) => {
   return (
     <li>
-      <Link href={url} className="h-full flex items-center justify-center" >
+      <Link href={url} className="flex h-full items-center justify-center">
         {children}
       </Link>
     </li>
