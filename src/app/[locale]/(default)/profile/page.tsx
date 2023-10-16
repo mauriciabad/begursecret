ecaa9bdf69839f98ae29c1a9029cad13f53404d9
mauriciabad <hello@mauriciabad.com>
2023-10-16 14:21:04 +0200

    Remove other login

--- src/app/[locale]/(default)/profile/page.tsx
+++ src/app/[locale]/(default)/profile/page.tsx
@@ -1,12 +1,8 @@
-
 import type { Metadata } from 'next'
 import { useTranslations } from 'next-intl'
 import { getTranslator } from 'next-intl/server'
 import type { FC } from 'react'
-import { AuthRequired } from '~/components/auth-required'
 import { LanguageSwitcher } from '~/components/language-switcher'
-import { SigninLink } from '~/components/signin-link'
-import { UserLogin } from '~/components/user-login'
 
 import type { LocaleRouteParams } from '~/i18n'
 
@@ -24,20 +20,7 @@ const ProfilePage: FC<LocaleRouteParams> = () => {
   const t = useTranslations('profile')
   return (
     <>
-      
       <p>{t('content')}</p>
-      <AuthRequired
-        fallback={
-          <div>
-            {t.rich('signIn', {
-              link: (chunks) => <SigninLink>{chunks}</SigninLink>,
-            })}
-          </div>
-        }
-      >
-        <p>{t('authenticated')}</p>
-      </AuthRequired>
-      <UserLogin />
 
       <LanguageSwitcher />
     </>
