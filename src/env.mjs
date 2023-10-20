541a5c7a4c68021c59beb1818a2d4aec497034b3
Maurici Abad Gutierrez <hello@mauriciabad.com>
2023-10-20 16:42:39 +0000

    remove is prod

--- src/env.mjs
+++ src/env.mjs
@@ -9,7 +9,6 @@ import { z } from 'zod'
 const server = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']),
   VERCEL_ENV: z.enum(['development', 'preview', 'production']).optional(),
-  IS_PRODUCTION: z.enum(['true', 'false']),
   PORT: z.preprocess((str) => {
     if (typeof str !== 'string') return undefined
     const port = parseInt(str, 10)
@@ -68,11 +67,6 @@ const vercelUrlWithProtocol =
 const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   VERCEL_ENV: process.env.VERCEL_ENV,
-  IS_PRODUCTION:
-    process.env.NODE_ENV === 'production' &&
-    process.env.VERCEL_ENV === 'production'
-      ? 'true'
-      : 'false',
   PORT: process.env.PORT,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
