050798f2a9bcdf0c14645f3c41107676ad68a5b3
Maurici Abad Gutierrez <hello@mauriciabad.com>
2023-10-20 14:14:28 +0000

    a

--- src/helpers/client-env.ts
+++ src/helpers/client-env.ts
@@ -1,5 +1,4 @@
 export const isProduction =
-  process.env.NODE_ENV === 'production' &&
-  window.location.hostname === process.env.VERCEL_URL
+  process.env.NODE_ENV === 'production' && Boolean(process.env.VERCEL_URL)
 
 export const isUsingLocalDb = process.env.USE_LOCAL_DB === 'true'
