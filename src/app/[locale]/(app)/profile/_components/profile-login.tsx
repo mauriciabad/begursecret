f4ef2cd802e9f8fb74d6a0530aff05b97fba362f
mauriciabad <hello@mauriciabad.com>
2023-10-21 13:46:54 +0200

    Create regster and banner components

--- src/app/[locale]/(app)/profile/_components/profile-login.tsx
+++ src/app/[locale]/(app)/profile/_components/profile-login.tsx
@@ -4,27 +4,14 @@ import { LinkButton } from '~/components/link-button'
 import { ContinueWithProvider } from './continue-with-provider'
 import { ContinueWithEmail } from './continue-with-email'
 import { env } from 'process'
+import { RegisterBanner } from './register-banner'
 
 export const ProfileLogin: FC = () => {
   const t = useTranslations('profile.login')
 
   return (
     <>
-      <div className="h-auto rounded-xl bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 bg-cover bg-center object-fill p-6 text-white shadow-lg shadow-purple-500/50">
-        <div className="mb-10 ">
-          <p className="text-sm font-bold uppercase">{t('banner.subtitle')}</p>
-          <h3 className="font-title text-3xl font-bold">{t('banner.title')}</h3>
-          <p className="text-lg leading-none">{t('banner.text')}</p>
-        </div>
-
-        <LinkButton
-          href="/register"
-          radius="full"
-          className="bg-white font-medium uppercase text-purple-500"
-        >
-          {t('register')}
-        </LinkButton>
-      </div>
+      <RegisterBanner />
 
       <div className="mx-auto mt-10  max-w-sm">
         <h2 className="mb-2 text-center font-title text-2xl font-semibold uppercase text-stone-800">
---
+++ src/app/[locale]/(app)/profile/_components/register-banner.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@nextui-org/button'
+import { useTranslations } from 'next-intl'
+import Link from 'next-intl/link'
+import { FC } from 'react'
+import { cn } from '~/helpers/cn'
+
+export const RegisterBanner: FC<{
+  className?: string
+}> = ({ className }) => {
+  const t = useTranslations('profile.register-banner')
+  return (
+    <Link
+      href="/register"
+      className="block h-auto rounded-xl bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 bg-cover bg-center object-fill p-6 text-white shadow-lg shadow-purple-500/50"
+    >
+      <div className={cn('mb-10', className)}>
+        <p className="text-sm font-bold uppercase">{t('subtitle')}</p>
+        <h3 className="font-title text-3xl font-bold uppercase">
+          {t('title')}
+        </h3>
+        <p className="text-lg leading-none">{t('text')}</p>
+      </div>
+
+      <Button
+        radius="full"
+        className="bg-white font-medium uppercase text-purple-500"
+        tabIndex={-1}
+      >
+        {t('register')}
+      </Button>
+    </Link>
+  )
+}
