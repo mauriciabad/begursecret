26ed80c74fc4bfce0c7f37babdd6ad5fae29e062
mauriciabad <hello@mauriciabad.com>
2023-10-22 20:09:06 +0200

    Update generated migrations

--- drizzle/0001_slow_stark_industries.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `account` MODIFY COLUMN `refresh_token` text;--> statement-breakpoint
-ALTER TABLE `account` MODIFY COLUMN `access_token` text;
--- drizzle/0002_massive_scream.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `user` MODIFY COLUMN `name` varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE `user` ADD `hashedPassword` varchar(255);--> statement-breakpoint
-ALTER TABLE `user` ADD CONSTRAINT `user_email_unique` UNIQUE(`email`);
