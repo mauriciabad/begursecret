c299115da0962f8d0c51195b949abf2e365f8118
mauriciabad <hello@mauriciabad.com>
2023-10-25 00:40:44 +0200

    Can be null

--- src/server/db/schema/places.ts
+++ src/server/db/schema/places.ts
@@ -3,5 +3,5 @@ import { mysqlTable, serial, text, varchar } from 'drizzle-orm/mysql-core'
 export const places = mysqlTable('place', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
-  mainImage: varchar('mainImage', { length: 1024 }).notNull(),
+  mainImage: varchar('mainImage', { length: 1024 }),
 })
