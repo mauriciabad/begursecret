3ec62529c42d8424af71dab108b82ed3edd854b2
mauriciabad <hello@mauriciabad.com>
2023-10-24 23:55:09 +0200

    Add main image

--- src/server/db/schema/places.ts
+++ src/server/db/schema/places.ts
@@ -1,6 +1,7 @@
-import { mysqlTable, serial, text } from 'drizzle-orm/mysql-core'
+import { mysqlTable, serial, text, varchar } from 'drizzle-orm/mysql-core'
 
 export const places = mysqlTable('place', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
+  mainImage: varchar('mainImage', { length: 1024 }).notNull(),
 })
