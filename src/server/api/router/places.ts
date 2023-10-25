6725cd08ed4614b4f6653f14eabebfe95f567d40
mauriciabad <hello@mauriciabad.com>
2023-10-25 22:50:39 +0200

    Fix get items not translated

--- src/server/api/router/places.ts
+++ src/server/api/router/places.ts
@@ -1,13 +1,12 @@
 import 'server-only'
 
-import { asc, eq, sql } from 'drizzle-orm'
+import { and, asc, eq, notExists, or, sql } from 'drizzle-orm'
 import { db } from '~/server/db/db'
 import { placesData, placesTranslations } from '~/server/db/schema/places'
 import { procedure, router } from '~/server/trpc'
 import { listPlacesSchema } from '~/schemas/places'
+import { defaultLocale } from '~/i18n'
 
-// TODO: Handle when a specific place doesn't have a translation for the given locale while others do.
-// Right now what happens is that the place without the locale is not returned in the list
 const getAllPlaces = db
   .select({
     id: placesData.id,
@@ -15,8 +14,26 @@ const getAllPlaces = db
     name: placesTranslations.name,
   })
   .from(placesData)
-  .innerJoin(placesTranslations, eq(placesData.id, placesTranslations.placeId))
-  .where(eq(placesTranslations.locale, sql.placeholder('locale')))
+  .leftJoin(placesTranslations, eq(placesData.id, placesTranslations.placeId))
+  .where(
+    or(
+      eq(placesTranslations.locale, sql.placeholder('locale')),
+      and(
+        eq(placesTranslations.locale, defaultLocale),
+        notExists(
+          db
+            .select()
+            .from(placesTranslations)
+            .where(
+              and(
+                eq(placesTranslations.placeId, placesData.id),
+                eq(placesTranslations.locale, sql.placeholder('locale'))
+              )
+            )
+        )
+      )
+    )
+  )
   .orderBy(asc(placesTranslations.name))
   .prepare()
 
