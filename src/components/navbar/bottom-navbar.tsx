e8853e53e8e7852fe2cfc52c941e21522698ba52
mauriciabad <hello@mauriciabad.com>
2023-10-29 16:55:05 +0100

    Fix accessibility problems

--- src/components/navbar/bottom-navbar.tsx
+++ src/components/navbar/bottom-navbar.tsx
@@ -14,11 +14,13 @@ import { getSession } from '~/server/get-server-thing'
 import { cn } from '~/helpers/cn'
 import { FC } from 'react'
 import { UserAvatar } from '../user-avatar'
+import { useTranslations } from 'next-intl'
 
 export const BottomNavbar: FC<{
   className?: string
 }> = async ({ className }) => {
   const session = await getSession()
+  const t = useTranslations('navbar')
 
   return (
     <nav
@@ -30,21 +32,25 @@ export const BottomNavbar: FC<{
       <ul className="mx-auto grid h-full max-w-2xl grid-cols-4">
         <BottomNavbarItem
           url="/explore"
+          label={t('explore')}
           icon={<IconCompass />}
           iconActive={<IconCompassFilled />}
         />
         <BottomNavbarItem
           url="/missions"
+          label={t('missions')}
           icon={<IconAward />}
           iconActive={<IconAwardFilled />}
         />
         <BottomNavbarItem
           url="/hub"
+          label={t('hub')}
           icon={<IconGift />}
           iconActive={<IconGiftFilled />}
         />
         <BottomNavbarItem
           url="/profile"
+          label={t('profile')}
           icon={
             session ? (
               <UserAvatar
