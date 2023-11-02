f584a44ce5b1fba8a7467c6343e913dcaec5b847
mauriciabad <hello@mauriciabad.com>
2023-11-02 20:38:38 +0100

    Add point SRID again

--- src/server/helpers/spatial-data.ts
+++ src/server/helpers/spatial-data.ts
@@ -10,7 +10,7 @@ export const pointType = customType<{
   driverData: { x: number; y: number }
 }>({
   dataType() {
-    return 'point'
+    return `POINT SRID ${SRID_CODE}`
   },
   toDriver(value: MapPoint | string) {
     const point = getPoint(value)
