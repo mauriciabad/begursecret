3a47379591610dad618a8f7e3246b86718f29c22
mauriciabad <hello@mauriciabad.com>
2023-11-03 04:45:31 +0100

    Fix ts nesting error

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -1,30 +1,23 @@
-import { Function, Object, String } from 'ts-toolbelt'
+import { F } from 'ts-toolbelt'
 
 export const groupByKey = <
-  O extends object,
-  P extends string,
-  PossibleNewKeys extends Object.Path<O, String.Split<P, '.'>>,
-  NewKeys extends PossibleNewKeys extends string | number
-    ? PossibleNewKeys
-    : never,
-  R extends { [key in NewKeys]: O[] },
+  T extends object,
+  K extends string,
+  R extends Record<string, T[]>,
 >(
-  array: O[],
-  key: Function.AutoPath<O, P>
+  array: T[],
+  key: F.AutoPath<T, K>
 ): R => {
   return array.reduce((acc, value) => {
-    const valueKey = getByNestedKey(value, key)
-    if (!(typeof valueKey === 'string' || typeof valueKey === 'number')) {
-      return acc
-    }
-    ;(acc[valueKey] ??= []).push(value)
+    const valueKey = String(
+      key
+        .split('.')
+        .reduce((nestedObject, k) => nestedObject?.[k], value as any)
+    ) as keyof R
+
+    acc[valueKey] ??= [] as unknown as R[keyof R]
+
+    acc[valueKey].push(value)
     return acc
   }, {} as R)
-}
-
-export function getByNestedKey<O extends object, P extends string>(
-  object: O,
-  path: Function.AutoPath<O, P>
-): Object.Path<O, String.Split<P, '.'>> {
-  return path.split('.').reduce((acc, key) => acc[key], object)
 }
