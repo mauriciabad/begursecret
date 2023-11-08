9be16214f4328a7bcab48f587d264441a4e034e9
mauriciabad <hello@mauriciabad.com>
2023-11-08 05:15:41 +0100

    Fix opacity

--- src/components/navbar/bottom-navbar-item.tsx
+++ src/components/navbar/bottom-navbar-item.tsx
@@ -18,7 +18,7 @@ export const BottomNavbarItem: FC<{
     <li>
       <LinkButton
         href={url}
-        className="flex h-full items-center justify-center"
+        className="opacity-1 flex h-full items-center justify-center"
         aria-label={label}
         variant="light"
         radius="none"
