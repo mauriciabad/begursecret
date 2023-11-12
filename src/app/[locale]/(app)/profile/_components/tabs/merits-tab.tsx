62d87d1501997d2466288bf7c504a13b188270b0
Maurici Abad Gutierrez <hello@mauriciabad.com>
2023-11-12 19:34:59 +0100

    Add visited places count in profile (#148)

--- src/app/[locale]/(app)/profile/_components/tabs/merits-tab.tsx
+++ src/app/[locale]/(app)/profile/_components/tabs/merits-tab.tsx
@@ -1,15 +1,36 @@
+import { IconLoader } from '@tabler/icons-react'
 import { useTranslations } from 'next-intl'
 import { FC } from 'react'
-import { UnderConstruction } from '~/components/generic/under-construction'
+import { trpc } from '~/trpc'
 
 export const MeritsTab: FC = () => {
   const t = useTranslations('profile.tabs.merits')
 
+  const { data: visitedPlacesCount } =
+    trpc.placeLists.getVisitedPlacesCount.useQuery()
+
   return (
     <>
-      <UnderConstruction />
-      <h2 className="text-center font-title font-medium">{t('tab-title')}</h2>
-      <p className="text-center">{t('under-construction')}</p>
+      <div className="flex justify-center">
+        <div className="flex-1 p-2 text-center leading-none">
+          {typeof visitedPlacesCount === 'number' ? (
+            <div className="text-4xl font-light">{visitedPlacesCount}</div>
+          ) : (
+            <IconLoader
+              className="mx-auto block h-10 animate-spin text-stone-400"
+              size={24}
+            />
+          )}
+          <div className="mt-1">
+            {t.rich('visited-places', {
+              part: (chunks) => <span className="block">{chunks}</span>,
+              highlight: (chunks) => (
+                <span className="font-semibold">{chunks}</span>
+              ),
+            })}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
