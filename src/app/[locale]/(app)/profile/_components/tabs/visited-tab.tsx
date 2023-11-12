a792b22d3c1b95339de4f7f262fd7d39b1303e4c
Maurici Abad Gutierrez <hello@mauriciabad.com>
2023-11-12 18:49:42 +0100

    Add visited places in profile (#147)
    * Add visited places in profile

---
+++ src/app/[locale]/(app)/profile/_components/tabs/visited-tab.tsx
@@ -0,0 +1,29 @@
+import { useLocale, useTranslations } from 'next-intl'
+import { FC } from 'react'
+import { onlyTranslatableLocales } from '~/i18n'
+import { trpc } from '~/trpc'
+import { PlaceList } from '../../../explore/_components/place-list'
+
+export const VisitedTab: FC = () => {
+  const t = useTranslations('profile.tabs.visited')
+  const locale = useLocale()
+  const { data: places } = trpc.placeLists.getVisitedPlaces.useQuery({
+    locale: onlyTranslatableLocales(locale),
+  })
+
+  return (
+    <>
+      <h2 className="text-center font-title font-medium">
+        {t('visited-places')}
+      </h2>
+
+      {!places ? (
+        <p className="mt-2 text-center text-gray-400">{t('loading')}</p>
+      ) : places.length === 0 ? (
+        <p className="mt-2 text-center text-gray-400">{t('no-places')}</p>
+      ) : (
+        <PlaceList places={places} />
+      )}
+    </>
+  )
+}
--- src/schemas/placeLists.ts
+++ src/schemas/placeLists.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod'
+import { translatableLocales } from '~/i18n'
 
 export const addToVisitedPlacesListSchema = z.object({
   placeId: z.number().int().min(0).optional(),
 })
 
+export const getVisitedPlacesSchema = z.object({
+  locale: z.enum(translatableLocales).nullable(),
+})
+
 export type AddToVisitedPlacesListSchemaType = z.infer<
   typeof addToVisitedPlacesListSchema
 >
+export type GetVisitedPlacesSchemaType = z.infer<typeof getVisitedPlacesSchema>
