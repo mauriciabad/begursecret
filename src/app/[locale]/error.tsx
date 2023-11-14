195a95e23e3942319fb51017888476af176e8587
Maurici Abad Gutierrez <hello@mauriciabad.com>
2023-11-14 17:27:49 +0100

    Fix 500 error page (#151)
    * Fix 500 error page
    
    * update imports

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -4,7 +4,6 @@ import { Button } from '@nextui-org/button'
 import { useTranslations } from 'next-intl'
 import { useEffect } from 'react'
 import { LinkButtonCustom } from '~/components/links/link-button-custom'
-import { env } from '~/env.mjs'
 
 export default function Error({
   error,
@@ -38,7 +37,7 @@ export default function Error({
               {error.digest}
             </p>
 
-            {env.NODE_ENV === 'development' && (
+            {process.env.NODE_ENV === 'development' && (
               <>
                 <p className="mb-2 text-lg font-light text-stone-500">
                   {t('error-message')}
