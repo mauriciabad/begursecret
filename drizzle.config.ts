a1c300bac2d4ad2c770f0bf37929de49b4d9e1ea
maui <hello@mauriciabad.com>
2024-01-23 17:56:59 +0100

    Update drizzle dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -32,6 +32,6 @@ export default {
           database: 'descobreix-begur-app',
         }
       : {
-          connectionString: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?ssl={"rejectUnauthorized":true}`,
+          uri: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?ssl={"rejectUnauthorized":true}`,
         },
 } satisfies Config
