6384f8e895a66b433ea05eac533694d9c1035619
maui <hello@mauriciabad.com>
2024-01-23 22:28:04 +0100

    Join migrations

--- drizzle/0012_sad_wilson_fisk.sql
+++ drizzle/0012_sad_wilson_fisk.sql
@@ -5,5 +5,5 @@ ALTER TABLE `placeToPlaceCategory` DROP PRIMARY KEY;--> statement-breakpoint
 ALTER TABLE `account` ADD PRIMARY KEY(`provider`,`providerAccountId`);--> statement-breakpoint
 ALTER TABLE `verificationToken` ADD PRIMARY KEY(`identifier`,`token`);--> statement-breakpoint
 ALTER TABLE `placeListToPlace` ADD PRIMARY KEY(`placeId`,`placeListId`);--> statement-breakpoint
-ALTER TABLE `placeToPlaceCategory` ADD PRIMARY KEY(`placeId`,`categoryId`);--> statement-breakpoint
+ALTER TABLE `placeToPlaceCategory` ADD PRIMARY KEY(`categoryId`,`placeId`);--> statement-breakpoint
 ALTER TABLE `user` ADD `role` varchar(255) DEFAULT 'user' NOT NULL;
