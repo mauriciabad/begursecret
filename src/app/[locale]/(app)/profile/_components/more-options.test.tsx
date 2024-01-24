7988cd8d630892d1e14d65fc5ce0138b17331662
Maurici Abad Gutierrez <hello@mauriciabad.com>
2024-01-24 18:43:08 +0100

    Fix intrgration tests (#158)

--- src/app/[locale]/(app)/profile/_components/more-options.test.tsx
+++ src/app/[locale]/(app)/profile/_components/more-options.test.tsx
@@ -1,21 +1,11 @@
-import { IconHelpCircle } from '@tabler/icons-react'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { MoreOptions } from '~/app/[locale]/(app)/profile/_components/more-options'
 import { render } from '~/test-utils'
-import { MoreOptionsModal, MoreOptionsModalButton } from './more-options-modal'
 
 describe('MoreOptionsModal', () => {
   it('shows the correct title', async () => {
-    render(
-      <MoreOptionsModal title="More options">
-        <MoreOptionsModalButton
-          url="/support"
-          text="Support"
-          icon={<IconHelpCircle />}
-          isExternal
-        />
-      </MoreOptionsModal>
-    )
+    render(<MoreOptions />)
 
     await userEvent.click(screen.getByRole('button', { name: 'More options' }))
 
