00d23582f13415523aee055cb0a7276c94adc4a9
maui <hello@mauriciabad.com>
2024-02-21 22:55:34 +0100

    Add ScrollShadow to place preview

--- src/app/[locale]/(app)/missions/_components/place-prevew-modal.tsx
+++ src/app/[locale]/(app)/missions/_components/place-prevew-modal.tsx
@@ -8,6 +8,7 @@ import {
   ModalHeader,
   ModalProps,
 } from '@nextui-org/modal'
+import { ScrollShadow } from '@nextui-org/scroll-shadow'
 import { IconChevronRight, IconInfoCircle } from '@tabler/icons-react'
 import { useTranslations } from 'next-intl'
 import { FC } from 'react'
@@ -47,50 +48,53 @@ export const PlacePreviewModal: FC<
               </ModalHeader>
               <ModalBody className="py-0">
                 <>
-                  <div className="-mx-6 overflow-x-auto px-6 scrollbar-hide">
-                    <div className="flex h-32 justify-start gap-2 [&>*]:shrink-0">
-                      <Link
-                        href={`/explore/places/${place.id}`}
-                        className="outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset-2"
-                      >
-                        <Map
-                          className="h-full w-24 rounded-md"
-                          zoom={isFar ? 10 : 11}
-                          center={
-                            isFar
-                              ? { lat: 41.94, lng: 3.18 }
-                              : { lat: 41.953, lng: 3.205 }
-                          }
-                          defaultLayer="bg-satelite-ign"
-                          markers={[
-                            {
-                              placeId: place.id,
-                              lat: place.location.lat,
-                              lng: place.location.lng,
-                              icon: place.mainCategory.icon,
-                              color: 'red',
-                              size: 'sm',
-                            },
-                          ]}
-                        />
-                      </Link>
+                  <ScrollShadow
+                    hideScrollBar
+                    orientation="horizontal"
+                    className="-mx-6 h-32 justify-start space-x-2 whitespace-nowrap leading-[0] [&>*:first-child]:!ml-6 [&>*:last-child]:!mr-6 [&>*]:inline-block"
+                    size={24}
+                  >
+                    <Link
+                      href={`/explore/places/${place.id}`}
+                      className="h-full w-24 outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset-2"
+                    >
+                      <Map
+                        className="h-full w-full rounded-md"
+                        zoom={isFar ? 10 : 11}
+                        center={
+                          isFar
+                            ? { lat: 41.94, lng: 3.18 }
+                            : { lat: 41.953, lng: 3.205 }
+                        }
+                        defaultLayer="bg-satelite-ign"
+                        markers={[
+                          {
+                            placeId: place.id,
+                            lat: place.location.lat,
+                            lng: place.location.lng,
+                            icon: place.mainCategory.icon,
+                            color: 'red',
+                            size: 'sm',
+                          },
+                        ]}
+                      />
+                    </Link>
+                    <OptimizedImage
+                      radius="md"
+                      full="height"
+                      image={place.mainImage}
+                      alt={place.name}
+                    />
+                    {place.images?.map((image) => (
                       <OptimizedImage
-                        radius="md"
+                        radius="lg"
+                        shadow="sm"
                         full="height"
-                        image={place.mainImage}
+                        image={image}
                         alt={place.name}
                       />
-                      {place.images?.map((image) => (
-                        <OptimizedImage
-                          radius="lg"
-                          shadow="sm"
-                          full="height"
-                          image={image}
-                          alt={place.name}
-                        />
-                      ))}
-                    </div>
-                  </div>
+                    ))}
+                  </ScrollShadow>
                   <div className="overflow-x-auto">
                     <CategoryTagList
                       mainCategory={place.mainCategory}
