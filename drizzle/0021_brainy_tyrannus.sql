3c6943134ae089620e4f995441b42c9910bb41ff
maui <hello@mauriciabad.com>
2024-02-22 23:44:11 +0100

    Add `hasInacurateLocation`

---
+++ drizzle/0021_brainy_tyrannus.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `feature` ADD `hasInacurateLocation` boolean;--> statement-breakpoint
+ALTER TABLE `feature` ADD `hasInacurateLocationNotes` text;--> statement-breakpoint
+ALTER TABLE `feature_translation` ADD `hasInacurateLocationNotes` text;
