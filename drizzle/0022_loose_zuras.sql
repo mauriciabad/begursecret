411ed5863219c3001de1ea02f26ed752dc1c01cd
maui <hello@mauriciabad.com>
2024-02-26 18:50:43 +0100

    Join db migrations

--- drizzle/0022_loose_zuras.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `feature` ADD `hasBench` boolean;
--- drizzle/0022_purple_scarlet_witch.sql
+++ drizzle/0022_purple_scarlet_witch.sql
@@ -8,6 +8,7 @@ ALTER TABLE `feature` ADD `depthMin` int;--> statement-breakpoint
 ALTER TABLE `feature` ADD `depthMax` int;--> statement-breakpoint
 ALTER TABLE `feature` ADD `scubaDivingLevel` enum('discoverScubaDiving','openWater','advancedOpenWater','specialtyDiver','technicalDiver');--> statement-breakpoint
 ALTER TABLE `feature` ADD `notThereAnymore` boolean;--> statement-breakpoint
+ALTER TABLE `feature` ADD `hasBench` boolean;--> statement-breakpoint
 ALTER TABLE `feature` ADD `howNarrow` enum('extremlyNarrow','narrow','extraSpace','wide','veryWide');--> statement-breakpoint
 ALTER TABLE `feature` ADD `hasMissingInfoNotes` text;--> statement-breakpoint
 ALTER TABLE `feature` ADD `notThereAnymoreNotes` text;--> statement-breakpoint
