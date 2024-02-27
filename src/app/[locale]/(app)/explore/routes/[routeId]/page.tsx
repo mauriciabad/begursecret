9bb6868f618bd69b5a57e98733254d79cd53d89e
maui <hello@mauriciabad.com>
2024-02-27 21:28:30 +0100

    Add metadata

--- src/app/[locale]/(app)/explore/routes/[routeId]/page.tsx
+++ src/app/[locale]/(app)/explore/routes/[routeId]/page.tsx
@@ -1,31 +1,66 @@
 import type { Metadata } from 'next'
-import { getTranslations } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 import { type FC } from 'react'
-import {
-  LocaleParams,
-  LocaleRouteParams,
-  onlyTranslatableLocales,
-} from '~/i18n'
+import { makeImageUrl } from '~/helpers/images'
+import { LocaleRouteParams, locales, onlyTranslatableLocales } from '~/i18n'
 import { getTrpc } from '~/server/get-server-thing'
 import { OverrideMainMap } from '../../_components/override-main-map'
 import { RouteDetails } from '../../_components/route-details'
 
-type Params = LocaleParams & { routeId: string }
+type Params = LocaleRouteParams<{ routeId: string }>
 
 export async function generateMetadata({
-  params: { locale },
-}: LocaleRouteParams): Promise<Metadata> {
-  const t = await getTranslations({ locale, namespace: 'route' })
+  params: { locale, routeId },
+}: Params): Promise<Metadata> {
+  const trpc = await getTrpc()
+  const route = await trpc.metadata.route({
+    locale: onlyTranslatableLocales(locale),
+    id: Number(routeId),
+  })
+  if (!route) return {}
+
+  const title = `${route.name}`
+  const categoriesList = [
+    route.mainCategory.namePlural,
+    ...route.categories.map((c) => c.category.namePlural),
+  ]
+  const description = `${route.description} | ${categoriesList.join(', ')}`
+  const alternates = Object.fromEntries(
+    locales.map((l) => [l, `/${l}/explore/routes/${routeId}`])
+  )
+
   return {
-    title: t('meta.title'),
-    description: t('meta.description'),
+    title,
+    description,
+    alternates: {
+      canonical: `/explore/routes/${routeId}`,
+      languages: alternates,
+    },
+    openGraph: {
+      title,
+      description,
+      url: alternates[locale],
+      siteName: 'Begur Secret',
+      images: route.mainImage
+        ? {
+            url: makeImageUrl(route.mainImage.key),
+            width: route.mainImage.width,
+            height: route.mainImage.height,
+            alt: route.mainImage.alt ?? undefined,
+          }
+        : undefined,
+      locale,
+      type: 'article',
+      // modifiedTime: route.updatedAt,
+      // publishedTime: route.updatedAt,
+      authors: 'Begur Secret',
+      tags: [...categoriesList, 'begur', 'costa-brava', 'baix-emporda'],
+      section: route.mainCategory.namePlural,
+    },
   }
 }
 
-const RoutePage: FC<{
-  params: Params
-}> = async ({ params }) => {
+const RoutePage: FC<Params> = async ({ params }) => {
   const routeId = Number(params.routeId)
 
   const trpc = await getTrpc()
