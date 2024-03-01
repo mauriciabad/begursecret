d56b8cf3c75fc66654bcfb612c0b4be564f39cf5
Maurici Abad Gutierrez <hello@mauriciabad.com>
2024-03-01 16:26:27 +0100

    Update src/app/[locale]/(app)/explore/places/[placeId]/page.tsx

--- src/app/[locale]/(app)/explore/places/[placeId]/page.tsx
+++ src/app/[locale]/(app)/explore/places/[placeId]/page.tsx
@@ -29,9 +29,6 @@ export async function generateMetadata({
     locales.map((l) => [l, `/${l}/explore/places/${placeId}`])
   )
 
-  // Wait 4 seconds
-  await new Promise((resolve) => setTimeout(resolve, 4000))
-
   return {
     title,
     description,
