415194273002b344244a2a1e1a6f9fc50b71733d
maui <hello@mauriciabad.com>
2024-03-06 19:27:45 +0100

    no await

--- src/server/helpers/translations/on-execute.ts
+++ src/server/helpers/translations/on-execute.ts
@@ -1,11 +1,10 @@
 export function doSomethingAfterExecute<
   P extends { execute: (...args: any[]) => Promise<any> },
   F extends (result: Awaited<ReturnType<P['execute']>>) => any,
-  Args extends Parameters<P['execute']>,
 >({ execute, ...preparedStatement }: P, doSomething: F) {
   return {
     ...preparedStatement,
-    execute: async (...args: Args) =>
-      (await doSomething(await execute(...args))) as ReturnType<F>,
+    execute: async (...args: Parameters<P['execute']>) =>
+      doSomething(await execute(...args)) as ReturnType<F>,
   }
 }
