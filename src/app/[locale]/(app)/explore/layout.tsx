873bd00fceac2399d7056d60a965a5d388fd2826
maui <hello@mauriciabad.com>
2024-03-19 22:09:50 +0100

    Display map when button clicked

--- src/app/[locale]/(app)/explore/layout.tsx
+++ src/app/[locale]/(app)/explore/layout.tsx
@@ -1,10 +1,9 @@
 import { unstable_setRequestLocale } from 'next-intl/server'
 import type { FC, PropsWithChildren } from 'react'
-import { MainMap } from '~/components/map/main-map'
 import { MainMapProvider } from '~/components/providers/main-map-provider'
-import { cn } from '~/helpers/cn'
 import { parseLocale, type LocaleRouteParams } from '~/i18n'
 import { ExploreTopbar } from './_components/explore-topbar'
+import { MainMapOrPlaceholder } from './_components/main-map-or-placeholder'
 import { MapDrawer } from './_components/map-drawer'
 
 type ExploreLayoutProps = PropsWithChildren<LocaleRouteParams>
@@ -19,14 +18,15 @@ const ExploreLayout: FC<ExploreLayoutProps> = async ({ params, children }) => {
 
       <main className="relative flex grow flex-col">
         <MainMapProvider>
-          <MainMap
-            className={cn(
-              'min-h-[calc(100dvh_-_65px_-_57px_-_57px)]',
-              'sticky top-16 grow',
-              '-mb-2 box-content pb-2'
-            )}
+          <MainMapOrPlaceholder
+            className="-mb-2 box-content pb-2"
             classNames={{
               controls: 'bottom-6',
+              expanded: [
+                'min-h-[calc(100dvh_-_65px_-_57px_-_57px)]',
+                'sticky top-16 grow',
+              ],
+              collapsed: 'h-48',
             }}
           />
 
