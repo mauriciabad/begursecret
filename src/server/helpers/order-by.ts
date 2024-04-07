fe02e6d9cf49c8d9c68e7ce1626231a1405f45e7
maui <hello@mauriciabad.com>
2024-04-07 21:40:56 +0200

    Nulls last

--- src/server/helpers/order-by.ts
+++ src/server/helpers/order-by.ts
@@ -1,17 +1,5 @@
 import { sql, type AnyColumn, type SQLWrapper } from 'drizzle-orm'
 
 export function ascNullsEnd(column: SQLWrapper | AnyColumn) {
-  return sql`ISNULL(${column}), ${column} asc`
-}
-
-export function descNullsEnd(column: SQLWrapper | AnyColumn) {
-  return sql`ISNULL(${column}), ${column} desc`
-}
-
-export function ascNullsStart(column: SQLWrapper | AnyColumn) {
-  return sql`NOT ISNULL(${column}), ${column} asc`
-}
-
-export function descNullsStart(column: SQLWrapper | AnyColumn) {
-  return sql`NOT ISNULL(${column}), ${column} desc`
+  return sql`${column} ASC NULLS LAST`
 }
