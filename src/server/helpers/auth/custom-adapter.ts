e2b76792975daa2b1a5b7be894c90881ceb47d6a
maui <hello@mauriciabad.com>
2024-04-07 19:55:54 +0200

    Fix CustomAdapter type

--- src/server/helpers/auth/custom-adapter.ts
+++ src/server/helpers/auth/custom-adapter.ts
@@ -1,10 +1,11 @@
 import 'server-only'
 
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { Adapter } from 'next-auth/adapters'
 import { db } from '../../db/db'
 import { initializeUserInDatabase } from './initialize-user'
 
-const tempAdapter = DrizzleAdapter(db)
-tempAdapter.createUser = initializeUserInDatabase
-
-export const CustomAdapter = tempAdapter
+export const CustomAdapter = {
+  ...(DrizzleAdapter(db) as Adapter),
+  createUser: initializeUserInDatabase,
+} satisfies Adapter
